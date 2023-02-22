99f6449634b4ca6131abd37e3261c78c152ab5b1
Danilo Canuto <anjos.danilo@gmail.com>
2023-02-22 09:43:06 -0300

    client registration with postman worked

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const authController = require("./controllers/authController");
 const app = express();
 
 // connect db
@@ -9,6 +10,12 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, () =>
   console.log("DB connection successful")
 );
+
+// routes and middlewares
+// the two middlewares below make req.body accessible to prevent them from returning undefined.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authController);
 
 // start server
 app.listen(process.env.PORT, () => console.log("Server started successfully"));
