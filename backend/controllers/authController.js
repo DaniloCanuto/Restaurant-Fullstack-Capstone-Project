05fa6d5c66e9a3669e06226e042506a02af07af9
Danilo Canuto <anjos.danilo@gmail.com>
2023-02-22 10:40:32 -0300

    authController concluded

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,5 +30,27 @@ authController.post("/register", async (req, res) => {
 });
 
 //login
+authController.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error("User information doesn't match!");
+    }
+    // normal passeord and encrypted password
+    const comparePass = await bcrypt.compare(req.body.password, user.password);
+    if (!comparePass) {
+      throw new Error("User information doesn't match!");
+    }
 
+    const { password, ...others } = user._doc;
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "5h" }
+    );
+    return res.status(200).json({ others, token });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
 module.exports = authController;
